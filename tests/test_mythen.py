85ba9f229880216c4cbf0f97fb5749183bfe450f
RJCD-Diamond <Richard.Dixey@diamond.ac.uk>
2026-01-28 20:33:15 +0000

    assert in settings test

--- tests/test_mythen.py
+++ tests/test_mythen.py
@@ -1,7 +1,9 @@
-from xrpd_toolbox.i11.mythen import MythenReductionSettings
-import numpy as np
-from xrpd_toolbox.utils.utils import gaussian, bin_and_propagate_errors
 import matplotlib.pyplot as plt
+import numpy as np
+
+from xrpd_toolbox.i11.mythen import MythenReductionSettings
+from xrpd_toolbox.utils.utils import bin_and_propagate_errors, gaussian
+
 
 def test_mythen_settings():
     settings = MythenReductionSettings(
@@ -18,22 +20,26 @@ def test_mythen_settings():
         error_calc="internal",
         data_reduction_mode=1,
         bad_channels_filepath="bad_channels.txt",
-        angcal_filepath="angcal.txt"
+        angcal_filepath="angcal.txt",
     )
-    
+
+    assert settings.apply_flatfield is True
+    assert settings.rebin_step == 2.0
+
+
 def test_mythen_settings_load_from_toml():
-    settings = MythenReductionSettings.load_from_toml("tests/test_data/mythen_settings.toml")
+    settings = MythenReductionSettings.load_from_toml(
+        "tests/test_data/mythen_settings.toml"
+    )
     assert settings.apply_flatfield is True
 
 
-
 def test_peak_averaging():
-
-    x1 = np.arange(0,10,0.1)
+    x1 = np.arange(0, 10, 0.1)
     y1 = gaussian(x1, amp=10.0, cen=5.0, fwhm=1.0, background=0.0)
     e1 = np.sqrt(y1)
 
-    x2 = np.arange(0.01,10.01,0.1)
+    x2 = np.arange(0.01, 10.01, 0.1)
     y2 = gaussian(x2, amp=20.0, cen=5.0, fwhm=1.0, background=0.0)
     e2 = np.sqrt(y2)
 
@@ -41,12 +47,15 @@ def test_peak_averaging():
     y_combined = np.concatenate((y1, y2))
     e_combined = np.concatenate((e1, e2))
 
-    x, y, e = bin_and_propagate_errors(x_combined, y_combined, e_combined, rebin_step=0.1, error_calc='internal')
+    x, y, e = bin_and_propagate_errors(
+        x_combined, y_combined, e_combined, rebin_step=0.1, error_calc="internal"
+    )
 
-    plt.errorbar(x1, y1, yerr=e1, fmt='o', label='Binned Data with Propagated Errors')
-    plt.errorbar(x2, y2, yerr=e2, fmt='o', label='Binned Data with Propagated Errors')
-    plt.errorbar(x-0.05, y, yerr=e, label='Binned Data with Propagated Errors')
+    plt.errorbar(x1, y1, yerr=e1, fmt="o", label="Binned Data with Propagated Errors")
+    plt.errorbar(x2, y2, yerr=e2, fmt="o", label="Binned Data with Propagated Errors")
+    plt.errorbar(x - 0.05, y, yerr=e, label="Binned Data with Propagated Errors")
     plt.show()
+
 
 if __name__ == "__main__":
     test_peak_averaging()
