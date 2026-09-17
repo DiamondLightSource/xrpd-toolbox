9d2b0007edda09fd768abbb368d46e65d733adae
akz63626 <richard.dixey@diamond.ac.uk>
2026-09-17 13:53:13 +0000

    move filesystem getting to file.py

--- src/xrpd_toolbox/utils/utils.py
+++ src/xrpd_toolbox/utils/utils.py
@@ -1,6 +1,5 @@
 import json
 import os
-import re
 import time
 from collections import defaultdict
 from collections.abc import Iterable, Sequence
@@ -155,44 +154,6 @@ def get_entry(filepath: str | Path) -> str:
 def dict_to_json(dict_to_save: dict, filepath: str | Path):
     with open(filepath, "w") as file:
         json.dump(dict_to_save, file, indent=4)
-
-
-def nexus_file_match(str_to_match, beamline: str = "i15-1"):
-    return re.match(f"{beamline}" + r"-+[0-9]+\.nxs", str_to_match)
-
-
-def get_nexus_files(
-    instrument_session_folder: str | Path,
-    beamline: str = "i15-1",
-    exclude: str = "processed",
-) -> list[str]:
-    """Get all final data files ending with .nxs in some folder"""
-
-    nexus_files = [
-        os.path.join(str(instrument_session_folder), f)
-        for f in os.listdir(instrument_session_folder)
-        if nexus_file_match(f, beamline) and (exclude not in f)
-    ]
-    nexus_files.sort()
-
-    return nexus_files
-
-
-def get_filenumber_from_nxs(filepath: str | Path) -> int:
-    basename = os.path.basename(str(filepath))
-    filenumber_str = re.findall(r"\d+", basename)[-1]
-    return int(filenumber_str)
-
-
-def get_folder_paths(root_folder: str | Path) -> list[str]:
-    """get all folder directories within another folder"""
-
-    instrument_session_folders = [
-        os.path.join(root_folder, f) for f in os.listdir(root_folder)
-    ]
-    instrument_session_folders.sort()
-
-    return instrument_session_folders
 
 
 def copy_datapath_to_nexus(
